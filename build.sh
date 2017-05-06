e8fcc51d3404676fb53fb600c239256cc265e594
floragunn <dev@search-guard.com>
2017-05-06 13:42:10 +0200

    cleanup, build more tags at once

--- build.sh
+++ build.sh
@@ -1,19 +1,28 @@
 #!/bin/bash
 set -e
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+TC_NATIVE_TAGS=(netty-tcnative-parent-2.0.0.Final netty-tcnative-parent-1.1.33.Fork25 netty-tcnative-1.1.33.Fork17)
+#for every os in OS list there must be subfolder with this name and Dockerfile in it
 OS=(alpine non-fedora fedora)
-cd $DIR
+MAVEN_VERSION=3.5.0
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd "$DIR"
 for i in "${OS[@]}"
 do
    : 
-   cd $DIR/$i
-   cp -a $DIR/scripts/compile.sh .
+   cd "$DIR/$i"
+   cp -a "$DIR/scripts/compile.sh" .
    echo "### BUILD $i ###"
-   docker build -t $i:latest . > /dev/null
+   docker build --build-arg "MAVEN_VERSION=$MAVEN_VERSION" -t "$i:latest" . > /dev/null
    rm -rf "$DIR/$i/binaries/gen"
-   echo "### RUN $i ###"
    mkdir -p "$DIR/$i/binaries"
-   docker run -e "NETTY_TCNATIVE_TAG=netty-tcnative-parent-2.0.0.Final" --rm -v $DIR/$i/binaries:/output "$i:latest"
+   for nv in "${TC_NATIVE_TAGS[@]}"
+   do
+     : 
+     echo "### RUN $i/$nv ###"
+     docker run -e "NETTY_TCNATIVE_TAG=$nv" --rm -v "$DIR/$i/binaries:/output" "$i:latest"
+     curl -T "$DIR/$i/binaries/gen/netty-tcnative-$nv-linux-x86_64.jar" -ufloragunncom:f6ada084a8e3a79ee07963ca5ab196e261386df9 "https://api.bintray.com/content/floragunncom/netty-tcnative/$i/$nv/nativejar/"
+     curl -T "$DIR/$i/binaries/gen/netty-tcnative-openssl-static-$nv-linux-x86_64.jar" -ufloragunncom:f6ada084a8e3a79ee07963ca5ab196e261386df9 "https://api.bintray.com/content/floragunncom/netty-tcnative/$i/$nv/nativejar/"
+   done 
 done
 
 cd $DIR
