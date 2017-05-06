398bcec3645003330c12f6c69ae0298366e8b17e
floragunn <dev@search-guard.com>
2017-05-06 18:44:44 +0200

    build all rc1

--- build.sh
+++ build.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 set -e
-TC_NATIVE_TAGS=(netty-tcnative-parent-2.0.0.Final netty-tcnative-parent-1.1.33.Fork25)
-# netty-tcnative-1.1.33.Fork17)
+############################################################################################
+############################################################################################
+OPENSSL_VERSION=1.0.2k
+OPENSSL_SHA256=6b3977c61f2aedf0f96367dcfb5c6e578cf37e7b8d913b4ecb6643c3cb88d8c0
+TC_NATIVE_TAGS=(netty-tcnative-1.1.33.Fork17 netty-tcnative-parent-2.0.0.Final netty-tcnative-parent-1.1.33.Fork25)
 #for every os in OS list there must be subfolder with this name and Dockerfile in it
 OS=(alpine non-fedora fedora)
-OS=(non-fedora fedora)
 MAVEN_VERSION=3.5.0
+############################################################################################
+############################################################################################
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 BUILDLOG="$DIR/build.log"
 DATE=`date +%Y-%m-%d:%H:%M:%S`
-echo $DATE
+echo "Start $DATE"
 rm -rf "$BUILDLOG"
 cd "$DIR"
 for i in "${OS[@]}"
@@ -24,7 +28,7 @@ do
    do
      : 
      echo "### RUN $i/$nv ###"
-     docker run -e "NETTY_TCNATIVE_TAG=$nv" --rm -v "$DIR/$i/binaries:/output" "$i:latest" >> "$BUILDLOG" 2>&1
+     docker run -e "NETTY_TCNATIVE_TAG=$nv" -e "OPENSSL_VERSION=$OPENSSL_VERSION" -e "OPENSSL_SHA256=$OPENSSL_SHA256" --rm -v "$DIR/$i/binaries:/output" "$i:latest" >> "$BUILDLOG" 2>&1
      VER=${nv##*-}
      echo "Upload files for $VER and $DATE"
      curl -T "$DIR/$i/binaries/gen/openssl-$nv/netty-tcnative-$VER-linux-x86_64.jar" -ufloragunncom:$BT_APIKEY "https://api.bintray.com/content/floragunncom/netty-tcnative/$i/$nv-$DATE/nativejar-$i-$VER-$DATE/"
--- scripts/compile.sh
+++ scripts/compile.sh
@@ -7,18 +7,20 @@ echo "" > "/output/gen/$(openssl version)"
 cat /usr/include/openssl/opensslv.h | grep OPENSSL_VERSION_TEXT > "/output/gen/openssl-header-version.txt"
 echo "$NETTY_TCNATIVE_TAG" > "/output/gen/$NETTY_TCNATIVE_TAG"
 mkdir -p "/output/gen/openssl-$NETTY_TCNATIVE_TAG"
-#mkdir -p /output/boringssl-static
-#mkdir -p /output/libressl-static
+#mkdir -p "/output/boringssl-$NETTY_TCNATIVE_TAG"
+#mkdir -p "/output/libressl-$NETTY_TCNATIVE_TAG"
 
 git clone https://github.com/netty/netty-tcnative || true
 cd netty-tcnative
 git checkout tags/$NETTY_TCNATIVE_TAG
-#sed -i -e 's#<module>openssl-static</module>#<!--<module>openssl-static</module>-->#g' pom.xml
-#sed -i -e 's#<module>openssl-dynamic</module>#<!--<module>openssl-dynamic</module>-->#g' pom.xml
-sed -i -e 's#<module>boringssl-static</module>#<!--<module>boringssl-static</module>-->#g' pom.xml
-sed -i -e 's#<module>libressl-static</module>#<!--<module>libressl-static</module>-->#g' pom.xml
-sed -i -e 's#<opensslVersion>1.0.2j</opensslVersion>#<opensslVersion>1.0.2k</opensslVersion>#g' pom.xml
-sed -i -e 's#<opensslSha256>e7aff292be21c259c6af26469c7a9b3ba26e9abaaffd325e3dccc9785256c431</opensslSha256>#<opensslSha256>6b3977c61f2aedf0f96367dcfb5c6e578cf37e7b8d913b4ecb6643c3cb88d8c0</opensslSha256>#g' pom.xml
+#sed -i -e 's#^\s*<module>openssl-static</module>\s*$#<!-- removed <module>openssl-static</module>-->#g' pom.xml
+#sed -i -e 's#^\s*<module>openssl-dynamic</module>\s*$#<!-- removed <module>openssl-dynamic</module>-->#g' pom.xml
+sed -i -e 's#^\s*<module>boringssl-static</module>\s*$#<!-- removed <module>boringssl-static</module>-->#g' pom.xml
+sed -i -e 's#^\s*<module>libressl-static</module>\s*$#<!--removed <module>libressl-static</module>-->#g' pom.xml
+sed -i -e 's#^\s*<opensslVersion>.*</opensslVersion>\s*$#<opensslVersion>'"$OPENSSL_VERSION"'</opensslVersion>#g' pom.xml
+sed -i -e 's#^\s*<opensslSha256>.*</opensslSha256>\s*$#<opensslSha256>'"$OPENSSL_SHA256"'</opensslSha256>#g' pom.xml
+
+cat pom.xml
 
 cat > docker_settings.xml <<EOL
 <settings xmlns="http://maven.apache.org/SETTINGS/1.0.0"
