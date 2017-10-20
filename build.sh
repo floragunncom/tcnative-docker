e0e7075b5810b4a246846ce5758bf2965f0e291e
floragunn <dev@search-guard.com>
2017-10-21 00:40:17 +0200

    build static libressl

--- build.sh
+++ build.sh
@@ -2,14 +2,14 @@
 set -e
 ############################################################################################
 ############################################################################################
-OPENSSL_VERSION=1.0.2l
-OPENSSL_SHA256=ce07195b659e75f4e1db43552860070061f156a98bb37b672b101ba6e3ddf30c
+LIBRESSL_VERSION=2.5.5
+LIBRESSL_SHA256=e57f5e3d5842a81fe9351b6e817fcaf0a749ca4ef35a91465edba9e071dce7c4
 #TC_NATIVE_TAGS=(netty-tcnative-1.1.33.Fork17 netty-tcnative-parent-2.0.0.Final netty-tcnative-parent-2.0.1.Final netty-tcnative-parent-1.1.33.Fork25 netty-tcnative-parent-1.1.33.Fork23)
 #TC_NATIVE_TAGS=(netty-tcnative-parent-2.0.2.Final netty-tcnative-parent-2.0.3.Final)
-TC_NATIVE_TAGS=(netty-tcnative-parent-2.0.6.Final)
+TC_NATIVE_TAGS=(netty-tcnative-parent-2.0.5.Final)
 
 #for every os in OS list there must be subfolder with this name and a Dockerfile in it
-OS=(alpine non-fedora fedora)
+OS=(non-fedora fedora alpine)
 MAVEN_VERSION=3.5.0
 ############################################################################################
 ############################################################################################
@@ -30,16 +30,18 @@ do
    for nv in "${TC_NATIVE_TAGS[@]}"
    do
      : 
-     echo "### RUN $i/$nv for Open SSL $OPENSSL_VERSION ###"
-     docker run -e "NETTY_TCNATIVE_TAG=$nv" -e "OPENSSL_VERSION=$OPENSSL_VERSION" -e "OPENSSL_SHA256=$OPENSSL_SHA256" --rm -v "$DIR/$i/binaries:/output" "$i:latest" >> "$BUILDLOG" 2>&1
+     echo "### RUN $i/$nv for Open SSL $LIBRESSL_VERSION ###"
+     docker run -e "NETTY_TCNATIVE_TAG=$nv" -e "LIBRESSL_VERSION=$LIBRESSL_VERSION" -e "LIBRESSL_SHA256=$LIBRESSL_SHA256" --rm -v "$DIR/$i/binaries:/output" "$i:latest" >> "$BUILDLOG" 2>&1
      VER=${nv##*-}
      echo "Upload files for $i/$VER"
      #PUT /content/:subject/:repo/:package/:version/:file_path[?publish=0/1][?override=0/1][?explode=0/1]
      #echo curl -T "$DIR/$i/binaries/gen/openssl-$nv/netty-tcnative-$VER-linux-x86_64.jar" -ufloragunncom:$BT_APIKEY "https://api.bintray.com/content/floragunncom/netty-tcnative/natives/$VER/netty-tcnative-openssl-1.0.2-dynamic-$VER-$i-linux-x86_64.jar?override=1"
-     curl -T "$DIR/$i/binaries/gen/openssl-$nv/netty-tcnative-$VER-linux-x86_64.jar" -ufloragunncom:$BT_APIKEY "https://api.bintray.com/content/floragunncom/netty-tcnative/natives/$VER/netty-tcnative-openssl-1.0.2-dynamic-$VER-$i-linux-x86_64.jar?override=1"
-     curl -T "$DIR/$i/binaries/gen/openssl-$nv/netty-tcnative-openssl-static-$VER-linux-x86_64.jar" -ufloragunncom:$BT_APIKEY "https://api.bintray.com/content/floragunncom/netty-tcnative/natives/$VER/netty-tcnative-openssl-$OPENSSL_VERSION-static-$VER-$i-linux-x86_64.jar?override=1"
+     #curl -T "$DIR/$i/binaries/gen/openssl-$nv/netty-tcnative-$VER-linux-x86_64.jar" -ufloragunncom:$BT_APIKEY "https://api.bintray.com/content/floragunncom/netty-tcnative/natives/$VER/netty-tcnative-libressl-$LIBRESSL_VERSION-dynamic-$VER-$i-linux-x86_64.jar?override=1"
+     curl -T "$DIR/$i/binaries/gen/libressl-$nv/netty-tcnative-libressl-static-$VER-linux-x86_64.jar" -ufloragunncom:$BT_APIKEY "https://api.bintray.com/content/floragunncom/netty-tcnative/natives/$VER/netty-tcnative-libressl-$LIBRESSL_VERSION-static-$VER-$i-linux-x86_64.jar?override=1"
      #POST /content/:subject/:repo/:package/:version/publish
-     curl -X POST -ufloragunncom:$BT_APIKEY "https://api.bintray.com/content/floragunncom/netty-tcnative/natives/$VER/publish"
+
+     #do not publish yet
+     #curl -X POST -ufloragunncom:$BT_APIKEY "https://api.bintray.com/content/floragunncom/netty-tcnative/natives/$VER/publish"
    done 
 done
 echo "All done"
--- scripts/compile.sh
+++ scripts/compile.sh
@@ -6,7 +6,7 @@ cd /output/gen
 echo "" > "/output/gen/$(openssl version)"
 cat /usr/include/openssl/opensslv.h | grep OPENSSL_VERSION_TEXT > "/output/gen/openssl-header-version.txt"
 echo "$NETTY_TCNATIVE_TAG" > "/output/gen/$NETTY_TCNATIVE_TAG"
-mkdir -p "/output/gen/openssl-$NETTY_TCNATIVE_TAG"
+mkdir -p "/output/gen/libressl-$NETTY_TCNATIVE_TAG"
 #mkdir -p "/output/boringssl-$NETTY_TCNATIVE_TAG"
 #mkdir -p "/output/libressl-$NETTY_TCNATIVE_TAG"
 
@@ -16,9 +16,10 @@ git checkout tags/$NETTY_TCNATIVE_TAG
 #sed -i -e 's#^\s*<module>openssl-static</module>\s*$#<!-- removed <module>openssl-static</module>-->#g' pom.xml
 #sed -i -e 's#^\s*<module>openssl-dynamic</module>\s*$#<!-- removed <module>openssl-dynamic</module>-->#g' pom.xml
 sed -i -e 's#^\s*<module>boringssl-static</module>\s*$#<!-- removed <module>boringssl-static</module>-->#g' pom.xml
-sed -i -e 's#^\s*<module>libressl-static</module>\s*$#<!--removed <module>libressl-static</module>-->#g' pom.xml
-sed -i -e 's#^\s*<opensslVersion>.*</opensslVersion>\s*$#<opensslVersion>'"$OPENSSL_VERSION"'</opensslVersion>#g' pom.xml
-sed -i -e 's#^\s*<opensslSha256>.*</opensslSha256>\s*$#<opensslSha256>'"$OPENSSL_SHA256"'</opensslSha256>#g' pom.xml
+sed -i -e 's#^\s*<module>openssl-static</module>\s*$#<!--removed <module>openssl-static</module>-->#g' pom.xml
+sed -i -e 's#^\s*<module>openssl-dynamic</module>\s*$#<!--removed <module>openssl-dynamic</module>-->#g' pom.xml
+sed -i -e 's#^\s*<libresslVersion>.*</libresslVersion>\s*$#<libresslVersion>'"$LIBRESSL_VERSION"'</libresslVersion>#g' pom.xml
+sed -i -e 's#^\s*<libresslSha256>.*</libresslSha256>\s*$#<libresslSha256>'"$LIBRESSL_SHA256"'</libresslSha256>#g' pom.xml
 
 cat pom.xml
 
@@ -33,7 +34,7 @@ EOL
 
 mvn -s docker_settings.xml clean package
 
-cp -av /output/gen/netty-tcnative/openssl-dynamic/target/*x86*.jar "/output/gen/openssl-$NETTY_TCNATIVE_TAG/"
-cp -av /output/gen/netty-tcnative/openssl-static/target/*x86*.jar "/output/gen/openssl-$NETTY_TCNATIVE_TAG/"
+#cp -av /output/gen/netty-tcnative/openssl-dynamic/target/*x86*.jar "/output/gen/openssl-$NETTY_TCNATIVE_TAG/"
+cp -av /output/gen/netty-tcnative/libressl-static/target/*.jar "/output/gen/libressl-$NETTY_TCNATIVE_TAG/"
 #mv boringssl-static/target/*.jar /output/boringssl-static
 #mv libressl-static/target/*.jar /output/libressl-static
