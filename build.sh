a900a2e5fa7fc4cbd3aba70476bde134643a6fdd
floragunn <dev@search-guard.com>
2018-03-28 16:15:06 +0200

    1.1.0h and tcnative 2.0.8

--- build.sh
+++ build.sh
@@ -2,11 +2,11 @@
 set -e
 ############################################################################################
 ############################################################################################
-OPENSSL_VERSION=1.0.2o
-OPENSSL_SHA256=ec3f5c9714ba0fd45cb4e087301eb1336c317e0d20b575a125050470e8089e4d
+OPENSSL_VERSION=1.1.0h
+OPENSSL_SHA256=5835626cde9e99656585fc7aaa2302a73a7e1340bf8c14fd635a62c66802a517
 #TC_NATIVE_TAGS=(netty-tcnative-1.1.33.Fork17 netty-tcnative-parent-2.0.0.Final netty-tcnative-parent-2.0.1.Final netty-tcnative-parent-1.1.33.Fork25 netty-tcnative-parent-1.1.33.Fork23)
 #TC_NATIVE_TAGS=(netty-tcnative-parent-2.0.2.Final netty-tcnative-parent-2.0.3.Final)
-TC_NATIVE_TAGS=(netty-tcnative-parent-2.0.7.Final netty-tcnative-parent-2.0.8.Final)
+TC_NATIVE_TAGS=(netty-tcnative-parent-2.0.8.Final)
 
 #for every os in OS list there must be subfolder with this name and a Dockerfile in it
 OS=(alpine non-fedora fedora)
