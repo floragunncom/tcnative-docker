b3750f33e50131ec1ab74d0d6af148ee31853e64
floragunn <dev@search-guard.com>
2018-11-24 22:29:48 +0100

    no err

--- build.sh
+++ build.sh
@@ -14,10 +14,11 @@ MAVEN_VERSION=3.5.0
 for i in "${OS[@]}"
 do
 	echo "#### BUILD Docker container for $i in $DIR ####"
-	ls -la "$DIR"
+	#ls -la "$DIR"
 	cd "$DIR/$i"
 	cp -a "$DIR/scripts/compile.sh" .
 	docker build --build-arg "MAVEN_VERSION=$MAVEN_VERSION" -t "$i:latest" . > /dev/null 2>&1
+	echo "Docker build: $?"
 	mkdir -p "$DIR/$i/binaries"
 
 	for OPENSSL_VERSION in "${OPENSSL_VERSIONS[@]}"
@@ -25,11 +26,13 @@ do
 		for nv in "${TC_NATIVE_TAGS[@]}"
 		do
 			BUILDLOG="$DIR/logs/run-$i-$nv-$OPENSSL_VERSION.log"
-			echo "  -- >RUN $i/$nv for Open SSL $OPENSSL_VERSION "
+			echo "  --> RUN $i/$nv for Open SSL $OPENSSL_VERSION"
+			rm -rf "$DIR/$i/binaries/gen"
 			docker run -e "NETTY_TCNATIVE_TAG=$nv" -e "OPENSSL_VERSION=$OPENSSL_VERSION" -e "OPENSSL_SHA256=$OPENSSL_SHA256" --rm -v "$DIR/$i/binaries:/output" "$i:latest" > "$BUILDLOG" 2>&1
+			echo "Docker run: $?"
 			VER=${nv##*-}
 			echo "    Upload files for $i/$VER"
-			ls -la "$DIR/$i/binaries/gen/openssl-$nv"
+			#ls -la "$DIR/$i/binaries/gen/openssl-$nv"
 			curl -T "$DIR/$i/binaries/gen/openssl-$nv/netty-tcnative-$VER-linux-x86_64.jar" -ufloragunncom:$BT_APIKEY "https://api.bintray.com/content/floragunncom/netty-tcnative/natives/$VER/netty-tcnative-openssl-$OPENSSL_VERSION-dynamic-$VER-$i-linux-x86_64.jar?override=1"
 			echo ""
 			curl -T "$DIR/$i/binaries/gen/openssl-$nv/netty-tcnative-openssl-static-$VER-linux-x86_64.jar" -ufloragunncom:$BT_APIKEY "https://api.bintray.com/content/floragunncom/netty-tcnative/natives/$VER/netty-tcnative-openssl-$OPENSSL_VERSION-static-$VER-$i-linux-x86_64.jar?override=1"
